4782e817cb011e5caf231464713e544b345954b7
pharmature <pharmature23@gmail.com>
2026-01-29 09:38:01 +0800

    test

--- cp1_fundament/s1_firstprogramme.py
+++ cp1_fundament/s1_firstprogramme.py
@@ -1,26 +1,9 @@
-# 第一个程序
+# 1.第一个程序
 print("Hello World")
 
-# 1. 数字类型
-# 1.1 整形int
-a = 10
-# 1.2 浮点型float
-b = 1.11
-# 1.3 复数complex
-c = complex(2)
-d = complex(2, 3)
-print(a, b, c, d)
-
-# 2. bool类型
-# 2.1 True
-age = 16
-print(age<=18)
-# 2.2 False
-print(age>=18)
-
-# 3. 字符串string
-str1 = 'hello world'
-print(str1)
-print(str1[1])
-print(str1[2: 6])
+# 2.输入输出
+# 输入 input
+name = input("Enter your name: ")
+# 输出 print
+print("Hello " + name)
 
---
+++ cp1_fundament/s3_data_class.py
@@ -0,0 +1,44 @@
+# 1. 数字类型 - 整形int 浮点型float 复数complex
+# a1 = 10
+# b1 = 1.11
+# c1 = complex(2)
+# d1 = complex(2, 3)
+# print(a1, b1, c1, d1)
+
+# 2. bool类型 - True False
+# age = 16
+# print(age <= 18)
+# print(age >= 18)
+
+# 3. 字符串string
+# str1 = 'hello world'
+# print(str1)
+# print(str1[1])
+# print(str1[2: 6])
+
+# 3.1 字符串的内置方法
+#
+#
+# # 格式化方法
+# # 1. 占位符
+# # 2. format()
+# # 3. f""
+#
+#
+#
+# 1. 列表
+
+
+# 2. 元组
+
+
+# 3. 字典
+
+
+
+
+
+
+
+
+
